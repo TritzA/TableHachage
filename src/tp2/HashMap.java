c313709aef8cb8c986cbcac7bb48c7542008e25f
Antonin Tritz <45083857+TritzA@users.noreply.github.com>
2020-09-27 18:00:20 -0400

    Initial commit

---
+++ src/tp2/HashMap.java
@@ -0,0 +1,161 @@
+package tp2;
+
+import java.util.Iterator;
+
+public class HashMap<KeyType, DataType> implements Iterable<KeyType> {
+
+    private static final int DEFAULT_CAPACITY = 20;
+    private static final float DEFAULT_LOAD_FACTOR = 0.5f;
+    private static final int CAPACITY_INCREASE_FACTOR = 2;
+
+    private Node<KeyType, DataType>[] map;
+    private int size = 0;
+    private int capacity;
+    private final float loadFactor; // Compression factor
+
+    public HashMap() { this(DEFAULT_CAPACITY, DEFAULT_LOAD_FACTOR); }
+
+    public HashMap(int initialCapacity) {
+        this(initialCapacity > 0 ? initialCapacity : DEFAULT_CAPACITY,
+                DEFAULT_LOAD_FACTOR);
+    }
+
+    public HashMap(int initialCapacity, float loadFactor) {
+        capacity = initialCapacity;
+        this.loadFactor = 1 / loadFactor;
+        map = new Node[capacity];
+    }
+
+    /**
+     * Finds the index attached to a particular key
+     * This is the hashing function ("Fonction de dispersement")
+     * @param key Value used to access to a particular instance of a DataType within map
+     * @return Index value where this key should be placed in attribute map
+     */
+    private int hash(KeyType key){
+        int keyHash = key.hashCode() % capacity;
+        return Math.abs(keyHash);
+    }
+
+    /**
+     * @return if map should be rehashed
+     */
+    private boolean needRehash() {
+        return size * loadFactor > capacity;
+    }
+
+    /**
+     * @return Number of elements currently in the map
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * @return Current reserved space for the map
+     */
+    public int capacity(){
+        return capacity;
+    }
+
+    /**
+     * @return if map does not contain any element
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /** TODO Average Case : O(1)
+     * Find the next prime after increasing the capacity by CAPACITY_INCREASE_FACTOR (multiplication)
+     */
+    private void increaseCapacity() {
+        // ...
+    }
+
+    /** TODO Average Case : O(n)
+     * Increases capacity by CAPACITY_INCREASE_FACTOR (multiplication) and
+     * reassigns all contained values within the new map
+     */
+    private void rehash() {
+        // ...
+    }
+
+    /** TODO Average Case : O(1)
+     * Finds if map contains a key
+     * @param key Key which we want to know if exists within map
+     * @return if key is already used in map
+     */
+    public boolean containsKey(KeyType key) { return false; }
+
+    /** TODO Average Case : O(1)
+     * Finds the value attached to a key
+     * @param key Key which we want to have its value
+     * @return DataType instance attached to key (null if not found)
+     */
+    public DataType get(KeyType key) {
+        return null;
+    }
+
+    /** TODO Average Case : O(1) , Worst case : O(n)
+     * Assigns a value to a key
+     * @param key Key which will have its value assigned or reassigned
+     * @return Old DataType instance at key (null if none existed)
+     */
+    public DataType put(KeyType key, DataType value) {
+        return null;
+    }
+
+    /** TODO Average Case : O(1)
+     * Removes the node attached to a key
+     * @param key Key which is contained in the node to remove
+     * @return Old DataType instance at key (null if none existed)
+     */
+    public DataType remove(KeyType key) {
+        return null;
+    }
+
+    /** TODO Worst Case : O(1)
+     * Removes all nodes contained within the map
+     */
+    public void clear() {
+        // ...
+    }
+
+    static class Node<KeyType, DataType> {
+        final KeyType key;
+        DataType data;
+        Node<KeyType, DataType> next; // Pointer to the next node within a Linked List
+
+        Node(KeyType key, DataType data)
+        {
+            this.key = key;
+            this.data = data;
+            next = null;
+        }
+    }
+
+    @Override
+    public Iterator<KeyType> iterator() {
+        return new HashMapIterator();
+    }
+
+    // Iterators are used to iterate over collections like so:
+    // for (Key key : map) { doSomethingWith(key); }
+    private class HashMapIterator implements Iterator<KeyType> {
+        // TODO: Add any relevant data structures to remember where we are in the list.
+
+        /** TODO Worst Case : O(n)
+         * Determine if there is a new element remaining in the hashmap.
+         */
+        public boolean hasNext() {
+            return false;
+        }
+
+        /** TODO Worst Case : O(n)
+         * Return the next new key in the hashmap.
+         */
+        public KeyType next() {
+            return null;
+        }
+    }
+}
---
+++ src/tp2/Interview.java
@@ -0,0 +1,22 @@
+package tp2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public final class Interview {
+
+    /** TODO Worst Case : O(n)
+     * Is valid if you flip the number upside down.
+     */
+    public static boolean isValidFlipped(String listOfNumbers) {
+        return false;
+    }
+
+    /** TODO Worst Case : O(n)
+     * Could be valid if you try to flip the number upside down with one of the combinations.
+     */
+    public static boolean isValidFlippedWithPermutation(String listOfNumbers) {
+        return false;
+    }
+}
